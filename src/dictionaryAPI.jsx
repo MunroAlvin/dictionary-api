b11c2069bda3021212827024218cba4160557802
MunroAlvin <munroalvin@gmail.com>
2022-09-17 13:25:32 +0300

    fetch, useEffect, and useState Example

--- src/dictionaryAPI.jsx
+++ src/dictionaryAPI.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function DictionaryAPI(){
 
+    const [users, setUsers] = useState(false)
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users").then(res => {
+            if(res.ok && res.status === 200){
+                return res.json();
+            }
+        })
+        .then(data => {setUsers(data);})
+        .catch(err => {console.log(err);})
+    }, []);
 
     return(
         <div>
-            <h1> Dictionary API </h1>
+            {users && users.map(user =>(
+                <li key={user.id}>
+                    {user.name}
+                </li>
+            ))}
         </div>
     );
 }
